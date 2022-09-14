ced98ef8f08e28fa3f83e75a904f6e075d56074e
kevinnchristian <kevin.cmribeiro@outlook.com>
2022-09-14 20:38:26 -0300

    implemente sign in screen initial

---
+++ src/screens/signIn/index.tsx
@@ -0,0 +1,16 @@
+import {
+  View,
+  Text,
+} from 'react-native';
+
+import { Button, Fields } from './style';
+
+const SignIn = () => {
+  return (
+    <View>
+      <Text>SignIn</Text>
+    </View>
+  );
+}
+
+export default SignIn;
---
+++ src/screens/signIn/style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native';
+
+const Button = styled.TouchableOpacity`
+`;
+
+const Fields = styled.TextInput`
+`;
+
+export { Button, Fields };
