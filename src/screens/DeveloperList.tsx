5241c02196446d6372f177a70aa60c76c3610df5
Alex Miguel <alexm@esparta.io>
2022-09-15 13:58:46 -0300

    developer list was created

--- src/screens/DeveloperList.tsx
+++ src/screens/DeveloperList.tsx
@@ -1,14 +1,17 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { FlatList } from 'react-native';
 import styled from 'styled-components/native';
+import { IDeveloper } from '../models/developer';
 import developerService from '../service/developer.service';
 
 const DeveloperList = () => {
+    const [developerList, setDeveloperList] = useState<IDeveloper[]>([]);
+
     useEffect(() => {
-        developerService.getAllDeveloper().then(reponse => console.log('reponse', reponse));
+        developerService.getAllDeveloper().then(setDeveloperList);
     }, []);
 
-    return <></>;
+    return <FlatList data={developerList} renderItem={item => <>{item.item.name}</>} />;
 };
 
 export default DeveloperList;
