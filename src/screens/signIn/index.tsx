b4df65d2f904a78a3fa012ace9c8011f4cc0fd80
kevinnchristian <kevin.cmribeiro@outlook.com>
2022-09-17 10:52:38 -0300

    initial implementation of sign in screen

--- src/screens/signIn/index.tsx
+++ src/screens/signIn/index.tsx
@@ -3,13 +3,65 @@ import {
   Text,
 } from 'react-native';
 
-import { Button, Fields } from './style';
+import {
+  Container,
+  ContentFieldsButtons,
+  ContentForgotPassword,
+  ContentSocialLogin,
+  Field,
+  ButtonLogin,
+  ButtonRegister,
+  ButtonLinkedIn,
+  ButtonGitHub,
+  TextBold,
+  TextBoldButton,
+} from './style';
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <Container>
+      <ContentFieldsButtons>
+        <TextBold>Email:</TextBold>
+        <Field />
+      </ContentFieldsButtons>
+
+      <ContentFieldsButtons>
+        <TextBold>Senha:</TextBold>
+        <Field />
+      </ContentFieldsButtons>
+
+      <ContentForgotPassword>
+        <TextBold>Esqueceu a senha</TextBold>
+      </ContentForgotPassword>
+
+      <ContentFieldsButtons>
+        <ButtonLogin>
+          <TextBoldButton>ENTRAR</TextBoldButton>
+        </ButtonLogin>
+      </ContentFieldsButtons>
+
+      <ContentFieldsButtons>
+        <ButtonRegister>
+          <TextBoldButton>CADASTRAR</TextBoldButton>
+        </ButtonRegister>
+      </ContentFieldsButtons>
+
+      <ContentSocialLogin>
+        <TextBold>CADASTRO SIMPLIFICADO</TextBold>
+
+        <ContentFieldsButtons>
+          <ButtonLinkedIn>
+            <TextBoldButton>LINKEDIN</TextBoldButton>
+          </ButtonLinkedIn>
+        </ContentFieldsButtons>
+
+        <ContentFieldsButtons>
+          <ButtonGitHub>
+            <TextBoldButton>GITHUB</TextBoldButton>
+          </ButtonGitHub>
+        </ContentFieldsButtons>
+      </ContentSocialLogin>
+    </Container>
   );
 }
 
