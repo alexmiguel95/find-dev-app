595e20d19418e1beb413e93c4825882ce106727a
kevinnchristian <kevin.cmribeiro@outlook.com>
2022-09-18 08:37:15 -0300

    implemente sign up screen initial

---
+++ src/screens/signUp/index.tsx
@@ -0,0 +1,18 @@
+import {
+  View,
+  Text,
+} from 'react-native';
+
+import {
+  Container,
+} from './style'
+
+const SignUp = () => {
+  return (
+    <Container>
+      <Text>Sign Up</Text>
+    </Container>
+  );
+}
+
+export default SignUp;
