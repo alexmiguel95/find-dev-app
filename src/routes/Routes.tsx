9a6245291e1e080ce9639c330230c7089d158bc9
kevinnchristian <kevin.cmribeiro@outlook.com>
2022-09-22 21:49:02 -0300

    add sign in and sign up screens to routes configuration

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,18 +1,27 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
 import DeveloperList from '../screens/DeveloperList';
+import SignIn from '../screens/signIn';
 import SignUp from '../screens/signUp';
 
 export type TypeRoute = {
     DeveloperList: undefined;
+    SignIn: undefined;
+    SignUp: undefined;
 };
 
 const { Navigator, Screen } = createStackNavigator();
 
 const Routes = () => {
     return (
-        <Navigator initialRouteName="LISTA">
-            <Screen name="LISTA" component={SignUp} />
-        </Navigator>
+        <NavigationContainer>
+            <Navigator initialRouteName="Entrar">
+                <Screen name="Lista" component={DeveloperList} />
+                <Screen name="Entrar" component={SignIn} />
+                <Screen name="Cadastrar" component={SignUp} />
+            </Navigator>
+        </NavigationContainer>
     );
 };
 
